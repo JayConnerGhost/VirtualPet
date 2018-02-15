bf18d98a4d3cb8986dc9176e692ad55e8ac443d1
digitalBitch <ghosttk@protonmail.com>
2018-02-15 18:02:01 +0000

    refactoring

--- VirtualPet.API.Tests/UnitTests/Models/LastFedTests.cs
+++ VirtualPet.API.Tests/UnitTests/Models/LastFedTests.cs
@@ -7,23 +7,6 @@ using Xunit;
 
 namespace VirtualPet.API.Tests.UnitTests.Models
 {
-    public class LastPettedTests
-    {
-        [Fact]
-        public void When_petted_animal_lastPetted_is_populated()
-        {
-            //arrange
-            IAnimal animal=new Animal();
-
-            //act
-            animal.Pet();
-           
-            //Assert
-            animal.LastPetted.Should().NotBeNull();
-
-        }
-    }
-
     public class LastFedTests
     {
         [Fact]
---
+++ VirtualPet.API.Tests/UnitTests/Models/LastPettedTests.cs
@@ -0,0 +1,23 @@
+﻿using FluentAssertions;
+using VirtualPet.Models;
+using Xunit;
+
+namespace VirtualPet.API.Tests.UnitTests.Models
+{
+    public class LastPettedTests
+    {
+        [Fact]
+        public void When_petted_animal_lastPetted_is_populated()
+        {
+            //arrange
+            IAnimal animal=new Animal();
+
+            //act
+            animal.Pet();
+           
+            //Assert
+            animal.LastPetted.Should().NotBeNull();
+
+        }
+    }
+}
